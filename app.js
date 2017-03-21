67e78bf1ee03da0043baee8f8f26509bdcaf3866
sue <sue@srcginc.com>
2017-03-21 11:56:54 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//------------------------------------------
+//   DOCUMENT READY
+//------------------------------------------*/
+
+$(document).ready(function() {
+  $(".js-like").on("click", function(event) {
+    event.preventDefault();
+
+    $(this).text("Liked!")
+      .closest(".juice-item")
+      .addClass("is-liked");
+  });
+
+  $(".js-add-link").on("click", function(event) {
+    event.preventDefault();
+
+    $(".js-form").toggleClass("is-visible");
+
+  });
+
+});
---
+++ mysqltest.php
@@ -0,0 +1,27 @@
+<?php
+ $db = mysqli_connect('localhost','root','22t!nk3r2','juice')
+ or die('Error connecting to MySQL server.');
+?>
+
+<html>
+ <head>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL</h1>
+
+ <?php
+ $query = "SELECT * FROM juice";
+ mysqli_query($db, $query) or die('Error querying database.');
+
+ $result = mysqli_query($db, $query);
+ $row = mysqli_fetch_array($result);
+
+ while ($row = mysqli_fetch_array($result)) {
+  echo $row['juicename'] . ' ' . $row['juicedescription'] . '<br />';
+ }
+
+ mysqli_close($db);
+ ?>
+
+</body>
+</html>
